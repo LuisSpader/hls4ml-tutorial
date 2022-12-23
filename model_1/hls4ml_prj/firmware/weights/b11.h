2923fe8e7a4289a6991ea0df560ab83c617f8639
Luis Antonio Spader Simon <luisspaders@gmail.com>
2022-12-23 10:07:54 -0300

    error permission denied

---
+++ model_1/hls4ml_prj/firmware/weights/b11.h
@@ -0,0 +1,15 @@
+//Numpy array shape [5]
+//Min -0.163121327758
+//Max 0.131709083915
+//Number of zeros 0
+
+#ifndef B11_H_
+#define B11_H_
+
+#ifndef __SYNTHESIS__
+model_default_t b11[5];
+#else
+model_default_t b11[5] = {0.1317090839, -0.1108367145, 0.0680375174, 0.0903269723, -0.1631213278};
+#endif
+
+#endif
